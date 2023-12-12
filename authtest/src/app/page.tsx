85ff2cdfd606859a5c3ab487385a124cf20f369e
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-12-12 10:31:16 +0000

    arema

--- authtest/src/app/page.tsx
+++ authtest/src/app/page.tsx
@@ -3,6 +3,7 @@ import PostForm from '@/components/form'
 import { SignIn, SignInButton, SignInWithMetamaskButton, SignOutButton, SignUp, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import submit from '@/PostSubmit'
+import PostData from '@/components/PostDataCard'
 
 export default async function Home() {
   return (
@@ -10,6 +11,8 @@ export default async function Home() {
       <h1 className='text-2xl'>Root Page</h1>
         <div className='container mx-auto p-4'>
           <PostForm /* submit={submit} *//>  
+          <div className='pt-5' />
+          <PostData />
         </div>
     </>
   )
---
+++ authtest/src/components/PostDataCard.tsx
@@ -0,0 +1,27 @@
+export default function PostData(){
+    //function PostCard(){
+        return (
+            <>
+                <article className="bg-white/30 rounded-lg pt-5 h-20">
+                    <div className="flex flex-row">
+                        <div className="avatar pl-3 basis-1/12 ">
+                            <div className="w-12 h-12 rounded-full">
+                                <img src="https://pbs.twimg.com/profile_images/1716981252400766976/CQNHPomm_400x400.jpg" alt="Tailwind-CSS-Avatar-component" />
+                            </div>
+                        </div>
+                        <div className="pl-5 basis-10/12">
+                                <div className="bg-white/10 rounded-lg">
+                                    <p className="h-[3.25rem] text-md">
+                                        真っ暗な空間の中で上半身だけコウメ太夫のコスプレした<br />
+                                        江頭2:50にﾁｸｼｮ~!!って叫ばれながら追いかけられる夢見た。
+                                        くそ怖かった
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+                </article>
+                
+            </>
+        )
+    // }
+}
